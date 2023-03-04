fafdfa3e137bea0e677599076751c314b13395c2
Denis Ignatov <dsignatov@licard.com>
2023-03-04 21:01:19 +0300

    feat: enable discovery client

--- src/main/java/github/denisspec989/productmainservice/ProductMainServiceApplication.java
+++ src/main/java/github/denisspec989/productmainservice/ProductMainServiceApplication.java
@@ -2,12 +2,14 @@ package github.denisspec989.productmainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableEurekaClient
 public class ProductMainServiceApplication {
 
 	public static void main(String[] args) {
